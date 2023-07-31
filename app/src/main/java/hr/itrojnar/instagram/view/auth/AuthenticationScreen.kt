01be720119b322768dd71631f8a14c83b8a4fec5
Ivan Trojnar <itrojnar@racunarstvo.hr>
2023-07-31 14:13:26 +0200

    added footer to log in screen

--- app/src/main/java/hr/itrojnar/instagram/view/auth/AuthenticationScreen.kt
+++ app/src/main/java/hr/itrojnar/instagram/view/auth/AuthenticationScreen.kt
@@ -43,6 +43,7 @@ import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -163,7 +164,7 @@ fun AuthenticationScreen(modifier: Modifier = Modifier) {
                     .padding(start = 20.dp, end = 20.dp),
                 onClick = { },
                 colors = ButtonDefaults.buttonColors(Color(0xFF3797EF)),
-                shape = RoundedCornerShape(10.dp)
+                shape = RoundedCornerShape(8.dp)
             ) {
                 Text(
                     stringResource(R.string.log_in),
@@ -173,15 +174,18 @@ fun AuthenticationScreen(modifier: Modifier = Modifier) {
             Row(
                 modifier = modifier
                     .fillMaxWidth()
-                    .padding(top = 35.dp)
+                    .padding(top = 40.dp)
                     .clickable { },
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center) {
+
                 Image(
                     painter = painterResource(id = R.drawable.google_icon),
                     contentDescription = stringResource(R.string.google_icon),
                     modifier = modifier.size(20.dp))
+
                 Spacer(modifier = modifier.width(8.dp))
+
                 Text(text = stringResource(R.string.log_in_with_google))
             }
             Row(
@@ -191,17 +195,20 @@ fun AuthenticationScreen(modifier: Modifier = Modifier) {
                     .clickable { },
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.Center) {
+
                 Image(
                     painter = painterResource(id = R.drawable.github_icon),
                     contentDescription = stringResource(R.string.github_icon),
                     modifier = modifier.size(20.dp))
+
                 Spacer(modifier = modifier.width(8.dp))
+
                 Text(text = stringResource(R.string.log_in_with_github))
             }
             Row(
                 modifier = modifier
                     .fillMaxWidth()
-                    .padding(horizontal = 20.dp, vertical = 25.dp),
+                    .padding(horizontal = 20.dp, vertical = 30.dp),
                 horizontalArrangement = Arrangement.Center,
                 verticalAlignment = Alignment.CenterVertically) {
 
@@ -247,6 +254,29 @@ fun AuthenticationScreen(modifier: Modifier = Modifier) {
                         fontSize = 16.sp
                     ),
                     onClick = {})
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(bottom = 16.dp),
+                verticalArrangement = Arrangement.Bottom,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                // Thin line
+                Spacer(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(1.dp)
+                        .background(Color.Black.copy(alpha = 0.2f))
+                )
+
+                // "Instagram or Facebook" text, centered
+                Text(
+                    text = stringResource(R.string.instagram_or_facebook),
+                    color = Color.Black.copy(alpha = 0.4f),
+                    modifier = Modifier.padding(top = 30.dp),
+                    textAlign = TextAlign.Center
+                )
             }
         }
     }
