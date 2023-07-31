f86d2d21909ddfdbcbb3cc71c13e0ad16d712241
Ivan Trojnar <itrojnar@racunarstvo.hr>
2023-07-31 09:40:28 +0200

    added log in button

--- app/src/main/java/hr/itrojnar/instagram/view/auth/AuthenticationScreen.kt
+++ app/src/main/java/hr/itrojnar/instagram/view/auth/AuthenticationScreen.kt
@@ -3,47 +3,41 @@ package hr.itrojnar.instagram.view.auth
 import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.TextFieldColors
-import androidx.compose.runtime.Composable
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import hr.itrojnar.instagram.R
@@ -70,77 +64,82 @@ fun AuthenticationScreen(modifier: Modifier = Modifier) {
         mutableStateOf("")
     }
 
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .background(Color.Transparent)) {
-        Image(
-            painter = painterResource(id = R.drawable.instagram_logo),
-            contentDescription = "Instagram logo",
-            contentScale = ContentScale.Fit,
-            modifier = Modifier.padding(start = 95.dp, top = 110.dp, end = 95.dp, bottom = 25.dp)
-        )
-        Surface(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(20.dp, 0.dp)
-                .background(Color.Transparent),
-            shape = RoundedCornerShape(10.dp),
-        ) {
-            OutlinedTextField(
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Transparent),
+    ) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .background(Color.Transparent)) {
+            Image(
+                painter = painterResource(id = R.drawable.instagram_logo),
+                contentDescription = "Instagram logo",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.padding(start = 95.dp, top = 110.dp, end = 95.dp, bottom = 25.dp)
+            )
+            Surface(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(0.dp, 0.dp, 0.dp, 5.dp),
-                value = emailState,
-                onValueChange = { emailState = it },
-                label = { Text(text = stringResource(R.string.email)) },
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Email,
-                    imeAction = ImeAction.Next
-                ),
-                keyboardActions = KeyboardActions (
-                    onDone = { focusManager.clearFocus() }
-                ),
-                colors = TextFieldDefaults.outlinedTextFieldColors(
-                    unfocusedLabelColor = Color.Gray, // Color for the hint text when not focused
-                    unfocusedBorderColor = Color.Gray, // Color for the border when not focused
-                    focusedLabelColor = Color.Black, // Color for the hint text when focused
-                    focusedBorderColor = Color.Black, // Color for the border when focused
+                    .padding(20.dp, 0.dp)
+                    .background(Color.Transparent),
+                shape = RoundedCornerShape(10.dp),
+            ) {
+                OutlinedTextField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(0.dp, 0.dp, 0.dp, 5.dp),
+                    value = emailState,
+                    onValueChange = { emailState = it },
+                    label = { Text(text = stringResource(R.string.email)) },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Email,
+                        imeAction = ImeAction.Next
+                    ),
+                    keyboardActions = KeyboardActions (
+                        onDone = { focusManager.clearFocus() }
+                    ),
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        unfocusedLabelColor = Color.Gray, // Color for the hint text when not focused
+                        unfocusedBorderColor = Color.Gray, // Color for the border when not focused
+                        focusedLabelColor = Color.Black, // Color for the hint text when focused
+                        focusedBorderColor = Color.Black, // Color for the border when focused
+                    )
                 )
-            )
-        }
-        Surface(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 20.dp, top = 0.dp, end = 20.dp, bottom = 0.dp)
-                .background(Color.Transparent),
-            shape = RoundedCornerShape(10.dp),
-        ) {
-            OutlinedTextField(
+            }
+            Surface(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(0.dp, 5.dp, 0.dp, 5.dp),
-                value = emailState,
-                onValueChange = { emailState = it },
-                label = { Text(text = stringResource(R.string.password)) },
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Password,
-                    imeAction = ImeAction.Done
-                ),
-                keyboardActions = KeyboardActions (
-                    onDone = { focusManager.clearFocus() }
-                ),
-                colors = TextFieldDefaults.outlinedTextFieldColors(
-                    unfocusedLabelColor = Color.Gray, // Color for the hint text when not focused
-                    unfocusedBorderColor = Color.Gray, // Color for the border when not focused
-                    focusedLabelColor = Color.Black, // Color for the hint text when focused
-                    focusedBorderColor = Color.Black, // Color for the border when focused
+                    .padding(start = 20.dp, top = 0.dp, end = 20.dp, bottom = 0.dp)
+                    .background(Color.Transparent),
+                shape = RoundedCornerShape(10.dp),
+            ) {
+                OutlinedTextField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(0.dp, 5.dp, 0.dp, 5.dp),
+                    value = emailState,
+                    onValueChange = { emailState = it },
+                    label = { Text(text = stringResource(R.string.password)) },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Password,
+                        imeAction = ImeAction.Done
+                    ),
+                    keyboardActions = KeyboardActions (
+                        onDone = { focusManager.clearFocus() }
+                    ),
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        unfocusedLabelColor = Color.Gray, // Color for the hint text when not focused
+                        unfocusedBorderColor = Color.Gray, // Color for the border when not focused
+                        focusedLabelColor = Color.Black, // Color for the hint text when focused
+                        focusedBorderColor = Color.Black, // Color for the border when focused
+                    )
                 )
-            )
+            }
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 85.dp, end = 2.dp),
+                    .padding(top = 10.dp, end = 20.dp, bottom = 25.dp),
                 horizontalArrangement = Arrangement.End
             ) {
                 ClickableText(
@@ -150,6 +149,17 @@ fun AuthenticationScreen(modifier: Modifier = Modifier) {
                         color = Color(0xFF3797EF)
                     )
                 )
+            }
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+                    .padding(start = 20.dp, end = 20.dp),
+                onClick = { },
+                colors = ButtonDefaults.buttonColors(Color(0xFF3797EF)),
+                shape = RoundedCornerShape(12.dp)
+            ) {
+                Text("Log in", color = Color.White)
             }
         }
     }
