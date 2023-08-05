160e710e612aec38e6e80273e60a0b7263b0e631
Ivan Trojnar <itrojnar@racunarstvo.hr>
2023-08-05 09:02:56 +0200

    added MainViewModel class

---
+++ app/src/main/java/hr/itrojnar/instagram/viewmodel/MainViewModel.kt
@@ -0,0 +1,22 @@
+package hr.itrojnar.instagram.viewmodel
+
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+    val visiblePermissionDialogQueue = mutableStateListOf<String>()
+
+    fun dismissDialog() {
+        visiblePermissionDialogQueue.removeLast()
+    }
+
+    fun onPermissionResult(
+        permission: String,
+        isGranted: Boolean
+    ) {
+        if(!isGranted) {
+            visiblePermissionDialogQueue.add(0, permission)
+        }
+    }
+}
