83f09682a1d1388af25a720762322bc533a4a270
Ivan Trojnar <itrojnar@racunarstvo.hr>
2023-08-15 20:33:41 +0200

    added DrawerItem file

---
+++ app/src/main/java/hr/itrojnar/instagram/view/drawer/DrawerItem.kt
@@ -0,0 +1,50 @@
+package hr.itrojnar.instagram.view.drawer
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun DrawerItem(icon: ImageVector, label: String, isSelected: Boolean, action: () -> Unit) {
+    val backgroundColor = if (isSelected) Color(0xFFDD2A7B) else Color.White
+    val contentColor = if (isSelected) Color.White else Color.Gray
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = action)
+            .padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(backgroundColor)
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = label,
+            tint = contentColor,
+            modifier = Modifier.padding(16.dp)
+        )
+
+        Spacer(Modifier.width(8.dp))
+
+        Text(
+            text = label,
+            color = contentColor,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
